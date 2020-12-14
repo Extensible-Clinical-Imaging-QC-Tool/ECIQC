8cc32266fbf5f0d5bbd9894e65601081929d0440
Emmanuelle BOurigault <emmanuelle.bourigault@wolfson.ox.ac.uk>
2020-12-14 13:19:32 +0000

    test

--- src/MetadataEditor.hpp
+++ src/MetadataEditor.hpp
@@ -1,19 +1,14 @@
 #ifndef METADATAEDITOR_H_
 #define METADATAEDITOR_H_
-#include<iostream>
+
 
 class MetadataEditor {
 
-OFString fpath;
-
 public:
     int x;
-    
-    MetadataEditor(OFString file_path, int xx = 23);
+    MetadataEditor(int xx = 23);
 
     void print();
-
-    void readFile();
 
 };
 
