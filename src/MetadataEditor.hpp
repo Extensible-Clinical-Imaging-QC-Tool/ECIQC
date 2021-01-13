a750a38b719aa22472b4658305b346079d016ed0
Emmanuelle Bourigault <wolf6016@.ox.ac.uk>
2021-01-13 14:33:56 +0000

    Revert "included call for exist method"
    This reverts commit 94ece5e44fef6b587681b85ac19bcecf2e42e908.

--- src/MetadataEditor.hpp
+++ src/MetadataEditor.hpp
@@ -1,36 +1,15 @@
 #ifndef METADATAEDITOR_H_
 #define METADATAEDITOR_H_
 
-#include "../dcmdata/apps/mdfdsman.cc"
-#include "../dcmdata/apps/mdfdsman.h"
-//#include <dcmtk/dcmdata/dcitem.h>
-class MetadataEditor : public MdfDatasetManager {
+
+class MetadataEditor {
+
 public:
-  // Constructor
-  MetadataEditor(OFString file_path);
+    int x;
+    MetadataEditor(int xx = 23);
 
-  OFCondition setTag(OFString str);
-  void setTag(DcmTagKey key);
-  DcmTagKey getTagKey();
-  OFString getTagString();
-  OFBool exists(OFBool searchIntoSub = OFFalse);
-  OFBool exists(OFString otherTagString, OFBool searchIntoSub = OFFalse);
-  OFBool exists(const DcmTagKey &key, OFBool searchIntoSub = OFFalse);
-  OFBool match(OFString str_expr, OFCondition &flag);
-  OFCondition modify(OFString newValue, OFBool only_overwrite);
-  OFCondition modify(OFString newValue, OFString otherTagString,
-                     OFBool only_overwrite);
-  OFCondition modify(OFString newValue, DcmTagKey otherTagKey,
-                     OFBool only_overwrite);
-  OFCondition copyTag(OFString TagFrom, OFString TagTo);
+    void print();
 
-  DcmDataset *dset;
-
-private:
-  OFCondition stringToKey(OFString str, DcmTagKey &key);
-
-  OFString tagString;
-  DcmTagKey tagKey;
 };
 
 #endif // METADATAEDITOR_H_
--- src/parser.cpp
+++ src/parser.cpp
@@ -4,11 +4,6 @@
 #include <fstream>
 #include <nlohmann/json.hpp>
 #include "Parser.hpp"
-#include "MetadataEditor.cpp"
-#include "MetadataEditor.hpp"
-#include <dcmtk/dcmpstat/dcmpstat.h>
-#include <regex>
-#include <string>
 
 
 
@@ -40,7 +35,7 @@ void Parser::initial_settings(std::stringstream configuration){
 
 
   
-        // access operators for checks 
+        // access edit operators and call the corresponding method
         for (const auto& el : j["0x0001"]["checks"]){
             
             
@@ -50,12 +45,8 @@ void Parser::initial_settings(std::stringstream configuration){
                 for (const auto& s : el["EQUAL"].items())
                 {
                     std::cout << s.key() << " : " << s.value() << std::endl;
-                    if (s.key() == s.value())
-                    {
-                        std::cout << "tag equals value" << std::endl;
-                    }
-                    
                 }
+                // call edit method for EQUAL operand
 
             }
             else
@@ -70,58 +61,13 @@ void Parser::initial_settings(std::stringstream configuration){
                 for (const auto& s : el["EXIST"].items())
                 {
                     std::cout << s.key() << " : " << s.value() << std::endl;
-
-                    //call EXIST method
-
-                    OFString path = "/home/sabsr3/Documents/Group_Software_Project/ECIQC/"
-                    "DICOM_Images/1-1copy.dcm";
-                    MetadataEditor obj{path};
-                    obj.setTag("(0010,0010)");
-                    obj.exists("(0010,0010)");
-
-                    // for example the action in action value is REJECT
-                    // call method edit with "REJECT" string input
-                    if (el.find("actionIfTrue") != el.end()){
-                        for (const auto& v : el["actionifTrue"].items()){
-                            //obj.edit(v.value()) // where v.value() = "REJECT"/"REMOVE"/"CLEAR"/"INSERT"/"APPEND"/"OVERWRITE"/"REJECT ALL"
-                        }
-           
-
-                    } else if(el.find("actionIfFalse") != el.end())
-                    {
-                        //
-                    }
-                    else{
-
-                    }
-
                 }
 
             }
             else
             {
                 cout << "EXIST operator not found " << endl;
-            }   
-
-            if (el.find("REGEX") != el.end())
-            {
-                cout << "REGEX operator is found" << endl;
-                for (const auto& s : el["EXIST"].items())
-                {
-                    std::cout << s.key() << " : " << s.value() << std::endl;
-
-                    //call exist method
-                    //MetadataEditor obj;
-                    //obj.match("0x0001")
-                }
-
-            }
-            else
-            {
-                cout << "REGEX operator not found " << endl;
-            }   
-
-            // access edit operators for actions     
+            }       
             
         }
         
