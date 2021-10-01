f007bd340035a11729839477a69d0d882bd147ec
EngEmmanuel <emmanueloladokun@hotmail.co.uk>
2021-10-01 16:18:16 +0100

    Some tests for the new parser

---
+++ tests/ParserTests.cpp
@@ -0,0 +1,25 @@
+#include "Parser.hpp"
+#include <iostream>
+#include "catch.hpp"
+
+DcmTagKey pNameTagKey = DCM_PatientName;
+OFString testPath = "test.dcm";
+OFString schemaPath = "../src/latestSchema.json";
+OFString pName;
+Parser pObj{schemaPath};
+
+TEST_CASE("Test parser object instantiation","[NP]") {
+    std::cout << pObj.base.begin().key();
+}
+
+TEST_CASE("Test parser dset setting","[NP]") {
+    pObj.setDicomDset(testPath);
+    DcmDataset* temp = pObj.currentDataset;
+    COUT << temp;
+    temp->print(COUT);
+
+}
+
+TEST_CASE("Test parser parsing","[NP]") {
+    pObj.run();
+}
