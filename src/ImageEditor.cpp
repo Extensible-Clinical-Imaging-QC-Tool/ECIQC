1398fe756112c21355b0cc87e308f147403a34de
Ollie Turnbull <oturnbull1@gmail.com>
2021-12-14 14:36:55 +0000

    Created Image Editror Files

---
+++ src/ImageEditor.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <cv.hpp>
+#include <dcmtk/dcmpstat/dcmpstat.h>
+#include <vector>
---
+++ src/ImageEditor.hpp
@@ -0,0 +1,16 @@
+#ifndef IMAGEEDITOR_H_
+#define IMAGEEDITOR_H_
+
+#include "mdfdsman.h"
+#include <dcmtk/dcmpstat/dcmpstat.h>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+class ImageEditor : public MdfDatasetManager {
+public: 
+
+
+
+}
+
+#endif // METADATAEDITOR_H_
