254cea2630d41e24f8f9a84d1a3f30aa795f2536
Ollie Turnbull <oturnbull1@gmail.com>
2021-12-16 10:32:30 +0000

    Image Editor with constructors

--- src/ImageEditor.hpp
+++ src/ImageEditor.hpp
@@ -5,12 +5,44 @@
 #include <dcmtk/dcmpstat/dcmpstat.h>
 #include <vector>
 #include <opencv2/opencv.hpp>
+// TODO Update with Elliot's Utility Class
+// #include "ImageUtility.h"
 
 class ImageEditor : public MdfDatasetManager {
 public: 
+    /** Default constructor
+     */
+    ImageEditor();
+
+    /** Constructor to handle the scenario where a path to
+     * a DICOM file is provided 
+     * @param file_path holds the path to the DICOM file
+     */
+    ImageEditor(OFString file_path);
+
+    /** Constructor to hand the scenario where a DICOM dataset
+     * is passed directly to the class
+     * @param dataset holds a pointer to a DICOM dataset
+     */
+    ImageEditor(DcmDataset* dataset);
+
+    /** Creates a DICOM dataset using the DICOM file at the specified 
+     * path
+     * @param file_path holds the path to the DICOM file
+     * @return a pointer to the created DcmDataset
+     */
+    DcmDataset* pathToDataset(OFString file_path);
 
 
 
+    
+private:
+
+    // Holds the dataset to be modified
+    DcmDataset *dset;
+    // The dataset image
+    cv::Mat datasetImage;
+    std::string foundText;
 }
 
-#endif // METADATAEDITOR_H_
+#endif // ImageEditor_H_
