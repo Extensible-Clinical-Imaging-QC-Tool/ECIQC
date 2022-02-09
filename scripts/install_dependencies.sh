5dc2ce8672a9ad440546e0e028ff683b0760f316
Elliot Barbeary <e.barbeary@outlook.com>
2022-02-09 11:04:48 +0000

    Include opencv and tesseract in dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,1 +1,2 @@
-sudo apt install build-essential cmake libboost-program-options-dev libdcmtk-dev
+sudo add-apt-repository -y ppa:alex-p/tesseract-ocr
+sudo apt install build-essential cmake libboost-program-options-dev libdcmtk-dev libtesseract-dev tesseract-ocr libopencv-dev
