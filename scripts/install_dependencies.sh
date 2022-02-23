549bf44ded7b80d645e6954432354f29637c0f7e
Elliot Barbeary <e.barbeary@outlook.com>
2022-02-23 12:23:59 +0000

    add autoaccept install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,2 @@
 sudo add-apt-repository -y ppa:alex-p/tesseract-ocr
-sudo apt install build-essential cmake libboost-program-options-dev libdcmtk-dev libtesseract-dev tesseract-ocr libopencv-dev
+sudo apt install build-essential cmake libboost-program-options-dev libdcmtk-dev libtesseract-dev tesseract-ocr libopencv-dev -y
