0a1e92f86bb5c523192a249fbb1a6babd93a338b
Elliot Barbeary <e.barbeary@outlook.com>
2022-09-14 18:28:58 +0100

    Reverted broken merged change to METests from main

--- tests/METests.cpp
+++ tests/METests.cpp
@@ -90,34 +90,6 @@ TEST_CASE("Test for SETTING tag key", "[ME]"){
   CHECK(meObj.setTag(nameTagString).good());
 }
 
-
-DcmFileFormat test_dfile;
-OFString test_filepath = "test.dcm";
-OFCondition test_dset_load = test_dfile.loadFile(test_filepath.c_str());
-DcmDataset* test_dset = test_dfile.getDataset();
-
-OFString test_filepath_fail = "testfail.dcm";
-
-TEST_CASE("Test for creating ME object using dataset", "[ME]") {
-  MetadataEditor meObj_test{test_dset};
-  meObj_test.setTag(nameTagString);
-  CHECK(meObj_test.exists().good());
-}
-
-TEST_CASE("Test for file opening fail", "[ME]") {
-
-  std::ostringstream oss;
-  std::streambuf* p_cout_streambuf = std::cout.rdbuf();
-  std::cout.rdbuf(oss.rdbuf());
-  MetadataEditor meObj_test{test_filepath_fail};
-
-  std::cout.rdbuf(p_cout_streambuf); // restore
-  // test your oss content...
-  CHECK(oss);
-  CHECK(oss.str() == "Loading file into dataset manager: testfail.dcm\nError loading file: No such file or directory");
-  std::cout << oss.str();
-}
-
 TEST_CASE("Test for CHECKING tag EXISTENCE","[ME]") {
 
   meObj.setTag(nameTagString);
