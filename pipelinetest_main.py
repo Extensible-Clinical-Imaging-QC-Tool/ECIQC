cc381d56d1217175d8e2198a672ea518da0bdb4b
Yiming Wei <weiym97@163.com>
2023-03-01 15:25:33 +0000

    Revise pipelinetest_main.py, but not successful

--- pipelinetest_main.py
+++ pipelinetest_main.py
@@ -4,24 +4,34 @@ import os
 from tests.pipelinetests import PipelineTests
 
 if __name__ == '__main__':
-    #pipelinetest = PipelineTests('./DICOM_Images/')
-    #pipelinetest.generate_file_list()
-    #pipelinetest.run_pipeline("./schema/useCase.json","./result/","./result_quarantine/")
-    
+    pipelinetest = PipelineTests('./DICOM_Images/')
+    pipelinetest.generate_file_list()
+    pipelinetest.run_pipeline("./schema/useCase.json","./result/","./result_quarantine/")
+    '''
+    import pydicom
+    file_list = ['1-01.dcm','1-1copy.dcm','1-003.dcm','1-004.dcm','1-005.dcm','1-006.dcm','test.dcm','test2.dcm','testtext.dcm']
+    for file in file_list:
+        ds = pydicom.dcmread('DICOM_Images/'+file)
+        print(ds.file_meta)
+        #ds= pydicom.read_file('DICOM_Images/' + file)
+        #print(ds)
+    '''
+    '''
     cmd_1 = "./build/exe/qctool --config-file='./schema/PipelineCase.json'"
     cmd_2 = "storescp 11113 --output-directory ./result/"
     cmd_3 = "storescp 11114 --output-directory ./result_quarantine/"
     cmd_4 = "storescu localhost 11112 DICOM_Images --scan-directories"
     cmd = cmd_1 + " & " + cmd_2 + " & " + cmd_3 + " & " + cmd_4
     os.system(cmd)
-
+    '''
+    '''
     ### Now we test the file
     import pydicom
     original_file = pydicom.read_file('DICOM_Images/1-1copy.dcm')
     result_file = pydicom.read_file('result/DX.1.3.6.1.4.1.14519.5.2.1.9999.103.2710019309218103448268301702692')
     print('Original name: ', original_file.PatientName)
     print('Postprocessed name: ',result_file.PatientName)
-    
+    '''
     
     '''
     Note: when we rerun the whole pipeline test,
