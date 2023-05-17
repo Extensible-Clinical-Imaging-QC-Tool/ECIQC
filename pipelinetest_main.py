ae77f04eb50a3439e735b1852287f54286308cfd
Yiming Wei <weiym97@163.com>
2023-05-17 10:04:46 +0100

    update 17th May 2023

--- pipelinetest_main.py
+++ pipelinetest_main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     #os.system('kill $(lsof -t -i:11112) & kill $(lsof -t -i:11113) & kill $(lsof -t -i:11114)')
     
     
-    pipelinetest = PipelineTests('./DICOM_Images/')
+    pipelinetest = PipelineTests('./DICOM_Images_test/')
     pipelinetest.generate_file_list()
     pipelinetest.run_pipeline("./schema/PipelineCase.json",
                               "./result/",
